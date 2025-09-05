7955eea766acc37bcce9ec643acbeaceb488cce0
KiefBC <kiefhay642@outlook.com>
2025-09-04 19:26:06 -0700

    chore: refactored response pkg
    - split response.go into headers.go and status_line.go

--- internal/response/headers.go
+++ internal/response/headers.go
@@ -7,33 +7,6 @@ import (
 	"github.com/kiefbc/http-server-1.1/internal/headers"
 )
 
-type StatusCode int
-
-const (
-	StatusOK                  StatusCode = 200
-	StatusBadRequest          StatusCode = 400
-	StatusInternalServerError StatusCode = 500
-)
-
-// WriteStatusLine writes an HTTP status line to the provided writer.
-// The status line format follows RFC 9112 Section 3.1.2: HTTP-version SP status-code SP [reason-phrase] CRLF
-func WriteStatusLine(w io.Writer, statusCode StatusCode) error {
-	switch statusCode {
-	case 200:
-		_, err := fmt.Fprintf(w, "HTTP/1.1 200 OK\r\n")
-		return err
-	case 400:
-		_, err := fmt.Fprintf(w, "HTTP/1.1 400 Bad Request\r\n")
-		return err
-	case 500:
-		_, err := fmt.Fprintf(w, "HTTP/1.1 500 Internal Server Error\r\n")
-		return err
-	default:
-		_, err := fmt.Fprintf(w, "HTTP/1.1 %d\r\n", statusCode)
-		return err
-	}
-}
-
 // GetDefaultHeaders creates a standard set of HTTP response headers.
 // Includes Content-Length, Connection, and Content-Type headers per RFC 9110 recommendations.
 func GetDefaultHeaders(contentLen int) headers.Headers {
